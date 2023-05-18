c4af272747d3c740c48fe3a5edb237b63763ff41
DarkAsfu <ashraful.islam.asfu@gmail.com>
2023-05-19 00:15:20 +0600

    404 page created

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,10 +1,12 @@
-
+import { Link } from 'react-router-dom';
+import error from '../../../src/assets/404.gif'
 
 const Error = () => {
     return (
-        <div>
-            <h1>404 </h1>
-        </div>
+            <div className='grid justify-center'>
+                <img src={error} alt="" />
+                <Link className=' w-36 mx-auto bg-blue-700 hover:bg-blue-900 text-white btn border-0' to='/'>Go to Home</Link>
+            </div>
     );
 };
 
