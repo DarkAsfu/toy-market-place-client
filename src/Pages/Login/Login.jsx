60bdfef225c6f75ef62ccd3ff4b11ade0636979b
DarkAsfu <ashraful.islam.asfu@gmail.com>
2023-05-18 22:50:17 +0600

    Login intregation complete

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,34 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import SocialLogin from "../Shared/SocialLogin/SocialLogin";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../Providers/AuthProvider";
 
 const Login = () => {
-    const handleSignIn = () => {
+    const { handleSignIn } = useContext(AuthContext);
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+    const navigate = useNavigate();
 
+    const signIn = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+        handleSignIn(email, password)
+        .then(result => {
+            const loggedUser = result.user;
+            console.log(loggedUser);
+            setSuccess('Successfully login!!');
+            setError('');
+            form.reset();
+            navigate("/")
+        })
+        .catch(error =>{
+            console.log(error.message);
+            setError(error.message);
+            setSuccess('')
+        })
     }
     return (
         <div className=''>
@@ -14,7 +39,7 @@ const Login = () => {
                         {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, officiis.</p> */}
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 pb-8">
-                        <form onSubmit={handleSignIn} className="card-body">
+                        <form onSubmit={signIn} className="card-body">
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
@@ -38,8 +63,8 @@ const Login = () => {
                         <p className="label-text-alt p-8">Are you new? please <Link to='/register' className='text-blue-400'>Register</Link></p>
                         {/* <SocialLogin></SocialLogin> */}
                         <SocialLogin></SocialLogin>
-                        {/* <small className='text-red-600 font-bold px-6 pb-3'>{error}</small>
-                <small className='text-green-600 font-bold px-6 pb-3'>{success}</small> */}
+                        <small className='text-red-600 font-bold px-6 pb-3'>{error}</small>
+                        <small className='text-green-600 font-bold px-6 pb-3'>{success}</small>
                     </div>
                 </div>
             </div>
