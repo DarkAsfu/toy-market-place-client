375f5c480acf9464ed0d4da8018e8356f4e824c5
DarkAsfu <ashraful.islam.asfu@gmail.com>
2023-05-19 04:40:44 +0600

    Extra one section added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../Banner/Banner";
 import Gallery from "../Gallery/Gallery";
+import TeamMember from "../TeamMember/TeamMember";
 
 const Home = () => {
     return (
         <>
             <Banner></Banner>
             <Gallery></Gallery>
+            <TeamMember></TeamMember>
         </>
     );
 };
