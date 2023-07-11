5eb15744f02a89a3cae9c49ebd2ea21524b90250
DarkAsfu <ashraful.islam.asfu@gmail.com>
2023-07-11 21:08:21 +0600

    Faq added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -3,6 +3,7 @@ import useTitle from "../../../hooks/useTitle";
 import Banner from "../Banner/Banner";
 import CategoryTab from "../CategoryTab/CategoryTab";
 import Contact from "../Contact/Contact";
+import Faq from "../Faq/Faq";
 import Gallery from "../Gallery/Gallery";
 import TeamMember from "../TeamMember/TeamMember";
 
@@ -15,6 +16,7 @@ const Home = () => {
             <Gallery></Gallery>
             <CategoryTab></CategoryTab>
             <TeamMember></TeamMember>
+            <Faq></Faq>
             <Contact></Contact>
         </>
     );
